8d4e93f8c2aa32c73d2ce214a660e0ffef3f1d34
fadyinvygo <158999457+fadyinvygo@users.noreply.github.com>
2024-08-21 16:18:50 +0300

    add test for dinner expense

--- expensereport-java/src/test/java/com/nelkinda/training/ExpenseReportTest.java
+++ expensereport-java/src/test/java/com/nelkinda/training/ExpenseReportTest.java
@@ -22,4 +22,24 @@ public class ExpenseReportTest {
 
         Assertions.assertEquals(expectedOutput, out.toString());
     }
+
+    @Test
+    public void testPrintReportWithOneExpense() {
+        Date date = new Date();
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        ExpenseReport expenseReport = new ExpenseReport(date);
+        Expense expense = new Expense();
+        expense.type = ExpenseType.DINNER;
+        expense.amount = 1000;
+
+        expenseReport.printReport(List.of(expense));
+
+        String expectedOutput = "Expenses " + date + "\n" +
+                "Dinner\t1000\t \n" +
+                "Meal expenses: 1000\n" +
+                "Total expenses: 1000\n";
+        Assertions.assertEquals(expectedOutput, out.toString());
+
+    }
 }
